2d960aabd5a272b5b72d785c238d8ce1729189ad
Diana Arreola <arreoladiana11@gmail.com>
2022-05-21 22:01:09 -0700

    add keyboard

--- wordle-clone-2/script.js
+++ wordle-clone-2/script.js
@@ -1,5 +1,45 @@
+const tileDisplay = document.querySelector('.tile-container')
+const keyboard = document.querySelector('.keyboard-container')
 
 // create keyboard
+const keys = [
+  'Q',
+  'W',
+  'E',
+  'R',
+  'T',
+  'Y',
+  'U',
+  'I',
+  'O',
+  'P',
+  'A',
+  'S',
+  'D',
+  'F',
+  'G',
+  'H',
+  'J',
+  'K',
+  'L',
+  'ENTER',
+  'Z',
+  'X',
+  'C',
+  'V',
+  'B',
+  'N',
+  'M',
+  '«',
+]
+
+keys.forEach(key => {
+  const buttonElement = document.createElement('button')
+  buttonElement.textContent = key
+  buttonElement.setAttribute('id', key)
+  keyboard.append(buttonElement)
+})
+
 
 // start and stop interactions
 // keyboard interaction + onclick interaction
