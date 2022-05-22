42dd33b15332ae1c064bc7025e6fa8234950c1aa
Diana Arreola <arreoladiana11@gmail.com>
2022-05-22 11:24:45 -0700

    handle click and keydown events

--- wordle-clone-2/script.js
+++ wordle-clone-2/script.js
@@ -37,9 +37,51 @@ keys.forEach(key => {
   const buttonElement = document.createElement('button')
   buttonElement.textContent = key
   buttonElement.setAttribute('id', key)
+  buttonElement.addEventListener('click', handleClick)
+  document.addEventListener('keydown', handleKeyPress)
   keyboard.append(buttonElement)
 })
 
+// startInteraction()
+// function startInteraction() {
+//   document.addEventListener('click', handleClick)
+//   document.addEventListener('keydown', handleKeyPress)
+// }
 
-// start and stop interactions
+function stopGame() {
+
+
+}
+
+function handleClick(e) {
+  if (e.target.textContent === 'ENTER') {
+    submitGuess()
+    return
+  }
+  if (e.target.textContent === '«') {
+    deleteKey()
+    return
+  }
+  console.log('clicked')
+}
+
+function handleKeyPress(e) {
+  if (e.key === 'ENTER') {
+    submitGuess()
+    return
+  }
+  if (e.key === '«') {
+    deleteKey()
+    return
+  }
+}
+
+function submitGuess() {
+  console.log('enter')
+  return
+}
+function deleteKey() {
+  console.log('delete')
+  return
+}
 // keyboard interaction + onclick interaction
